da85cc05b7a55168840079702a6d1c6a367edc7b
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2019-09-17 15:30:27 -0400

    first commit

---
+++ src/pages/box.js
@@ -0,0 +1,39 @@
+import React, { useState, useRef } from "react"
+import { Canvas, useRender } from "react-three-fiber"
+import { useSpring, a } from "react-spring/three"
+
+import "./style.css"
+
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? `#bada55` : `gray`,
+  })
+
+  const meshRef = useRef()
+
+  useRender(() => {
+    meshRef.current.rotation.y += 0.01
+  })
+
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
+  )
+}
+
+export default () => (
+  <Canvas>
+    <Box />
+  </Canvas>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,77 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useRef } from "react"
+import * as THREE from "three"
+import { Canvas, extend, useRender, useThree } from "react-three-fiber"
+import { useSpring, a } from "react-spring/three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "./style.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+extend({ OrbitControls })
+
+const Controls = () => {
+  const orbitRef = useRef()
+  const { camera, gl } = useThree()
+
+  // called on every frame
+  useRender(() => {
+    orbitRef.current.update()
+  })
+
+  return (
+    <orbitControls
+      autoRotate
+      //prevent vertical rotation:
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
+  )
+}
+
+const Plane = () => (
+  <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+    <planeBufferGeometry attach="geometry" args={[100, 100]} />
+    <meshPhysicalMaterial attach="material" color="white" />
+  </mesh>
 )
 
-export default IndexPage
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? `#bada55` : `gray`,
+  })
+
+  return (
+    <a.mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+      castShadow
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      {/* <a.meshBasicMaterial attach="material" color={props.color} /> */}
+      <a.meshPhysicalMaterial attach="material" color={props.color} />
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} penumbra={1} castShadow />
+    </a.mesh>
+  )
+}
+
+export default () => (
+  <Canvas
+    camera={{ position: [0, 0, 5] }}
+    onCreated={({ gl }) => {
+      gl.shadowMap.enabled = true
+      gl.shadowMap.type = THREE.PCFSoftShadowMap
+    }}
+  >
+    <fog attach="fog" args={["white", 5, 15]} />
+    <Box />
+    <Controls />
+    <Plane />
+  </Canvas>
+)
